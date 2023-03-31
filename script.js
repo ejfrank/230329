006ca9d16acda10f0076d968f9e0a333da742b0e
Erskine Frank <erskine@Erskines-MBP.gci.net>
2023-03-30 19:55:17 -0800

    updated to make the buttons work yay

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const phoneButton = document.getElementById('ring');
 const phoneMessage = document.getElementById('pop');
 const cakeButton = document.getElementById('bake');
-
+const readButton = document.getElementById('read');
+const gameButton = document.getElementById('game');
 // event listenerzzzz
 phoneButton.addEventListener('click', toggleRing);
 cakeButton.addEventListener('click', toggleBake);
-
+gameButton.addEventListener('click', toggleGame);
+readButton.addEventListener('click', toggleRead);
 function toggleRing() {
     phoneMessage.classList.toggle('hidden');
     if (phoneMessage.classList.contains('hidden')) {
@@ -20,5 +22,21 @@ function toggleBake() {
         phoneMessage.textContent = 'Phone a friend';
     } else {
         phoneMessage.textContent = 'BAKE UR HEART OUT';
+    }
+}
+function toggleRead() {
+    phoneMessage.classList.toggle('hidden');
+    if (phoneMessage.classList.contains('hidden')) {
+        phoneMessage.textContent = 'Phone a friend';
+    } else {
+        phoneMessage.textContent = 'reading is nice';
+    }
+}
+function toggleGame() {
+    phoneMessage.classList.toggle('hidden');
+    if (phoneMessage.classList.contains('hidden')) {
+        phoneMessage.textContent = 'Phone a friend';
+    } else {
+        phoneMessage.textContent = 'play a game on a board';
     }
 }
